700d3406fd5bb7f7f274298cfbc22dbf4043fb1a
ShahadatShobuj <shobujlingdu@gmail.com>
2023-09-02 18:02:27 +0600

    README.md updated

--- views/includes/header.blade.php
+++ views/includes/header.blade.php
@@ -1,4 +1,4 @@
 <div class="flex justify-between text-right items-center px-6 py-4 w-full">
-    <img src="{{ asset('favicon.png') }}" alt="" class="max-h-16">
-    <a href="{{ url('/employees') }}" class="text-sm text-white underline vertical">Employees</a>
+    <img src="{{ asset('singularity-crud/logo.png') }}" alt="" class="max-h-16">
+    <a href="{{ url('/') }}" class="text-sm text-white underline vertical">Home</a>
 </div>
